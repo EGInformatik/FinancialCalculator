59b7074e531cb5ef27c1bbfea080518907eeeccf
Alex Uldahl Pedersen <alex@uldahl.dk>
2020-09-24 16:37:14 +0200

    Initial commit

---
+++ src/models/FinancialModels.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+
+/**
+ *
+ * @author alex
+ */
+public class FinancialModels {
+
+    public double calculateInterest(double futureCapital, double presentCapital, double periods) {
+        return 0;
+    }   
+    
+    public double calculatePresentCapital(double futureCapital, double interest, double periods) {
+        return 0;
+    }
+    
+    public double calculateFutureCapital(double presentCapital, double interest, double periods) {
+        return 0;
+    }
+    
+    public double calculatePeriods(double futureCapital, double presentCapital, double interest) {
+        return 0;
+    }
+}
